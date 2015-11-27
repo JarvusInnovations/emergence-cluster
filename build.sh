3f4e85f7e9821dacad53560352dcae2b94e53952
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-11-27 13:40:35 -0500

    Try using a dist directory for Travis

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+mdkir dist
+
 echo "OSX releases work" > osx.txt
-zip emergence-osx-release.zip osx.txt
+zip dist/emergence-osx-release.zip osx.txt
 
 echo "LINUX releases work" > linux.txt
-zip emergence-osx-release.zip linux.txt
+zip dist/emergence-osx-release.zip linux.txt
